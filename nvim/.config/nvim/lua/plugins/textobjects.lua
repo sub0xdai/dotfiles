c50874402c139963aca4d0d5098b86e5727f619d
sub0xdai <sub0xdai@proton.me>
2026-06-10 09:50:56 +1000

    edit

--- nvim/.config/nvim/lua/plugins/textobjects.lua
+++ nvim/.config/nvim/lua/plugins/textobjects.lua
@@ -1,52 +1,78 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
-    lazy = false, -- must load after treesitter, and treesitter is lazy=false
-    priority = 900, -- after treesitter (1000 default), before most plugins
+    lazy = false,
     config = function()
-      -- merge textobjects into the already-initialized treesitter setup
-      require("nvim-treesitter").setup({
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["a["] = "@block.outer",
-              ["i["] = "@block.inner",
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]c"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[c"] = "@class.outer",
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              [">f"] = "@function.outer",
-              [">a"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<f"] = "@function.outer",
-              ["<a"] = "@parameter.inner",
-            },
-          },
+      local ts_textobjects = require("nvim-treesitter-textobjects")
+
+      ts_textobjects.setup({
+        select = {
+          lookahead = true,
+          include_surrounding_whitespace = false,
+        },
+        move = {
+          set_jumps = true,
         },
       })
+
+      -- Select textobjects
+      local select = require("nvim-treesitter-textobjects.select")
+      vim.keymap.set({ "x", "o" }, "af", function()
+        select.select_textobject("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "if", function()
+        select.select_textobject("@function.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        select.select_textobject("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        select.select_textobject("@class.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "a[", function()
+        select.select_textobject("@block.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "i[", function()
+        select.select_textobject("@block.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "aa", function()
+        select.select_textobject("@parameter.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ia", function()
+        select.select_textobject("@parameter.inner", "textobjects")
+      end)
+
+      -- Move textobjects
+      local move = require("nvim-treesitter-textobjects.move")
+      vim.keymap.set({ "n", "x", "o" }, "]f", function()
+        move.goto_next_start("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[f", function()
+        move.goto_previous_start("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "]c", function()
+        move.goto_next_start("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[c", function()
+        move.goto_previous_start("@class.outer", "textobjects")
+      end)
+
+      -- Swap textobjects
+      local swap = require("nvim-treesitter-textobjects.swap")
+      vim.keymap.set("n", ">f", function()
+        swap.swap_next("@function.outer")
+      end)
+      vim.keymap.set("n", "<f", function()
+        swap.swap_previous("@function.outer")
+      end)
+      vim.keymap.set("n", ">a", function()
+        swap.swap_next("@parameter.inner")
+      end)
+      vim.keymap.set("n", "<a", function()
+        swap.swap_previous("@parameter.inner")
+      end)
     end,
   },
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,51 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter").setup({
-        ensure_installed = {
-          "lua",
-          "typst",
-          "go",
-          "rust",
-          "python",
-          "javascript",
-          "typescript",
-          "c",
-          "cpp",
-          "bash",
-          "zig",
-          "haskell",
-          "latex",
-          "json",
-          "yaml",
-          "toml",
-          "html",
-          "css",
-          "sql",
-          "prisma",
-          "gleam",
-          "nim",
-          "wgsl",
+      require("nvim-treesitter").setup()
+
+      -- Install parsers (async, no-op if already installed)
+      require("nvim-treesitter").install({
+        "lua",
+        "typst",
+        "go",
+        "rust",
+        "python",
+        "javascript",
+        "typescript",
+        "c",
+        "cpp",
+        "bash",
+        "zig",
+        "haskell",
+        "latex",
+        "json",
+        "yaml",
+        "toml",
+        "html",
+        "css",
+        "sql",
+        "prisma",
+        "gleam",
+        "nim",
+        "wgsl",
+      })
+
+      -- Enable treesitter highlighting per filetype via autocommand
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "lua", "typst", "go", "rust", "python", "javascript",
+          "typescript", "c", "cpp", "bash", "zig", "haskell",
+          "latex", "json", "yaml", "toml", "html", "css", "sql",
+          "prisma", "gleam", "nim", "wgsl",
+          "markdown", "markdown_inline", "vim",
         },
-        auto_install = false,
-        highlight = { enable = true },
-        indent = { enable = false },
+        callback = function()
+          vim.treesitter.start()
+        end,
       })
     end,
   },
